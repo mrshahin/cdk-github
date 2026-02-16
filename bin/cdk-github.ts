a4d983317ac40b6e15bd4bd8470e3ad9bc74e04c
Shahin <shahin3213@gmail.com>
2026-02-16 11:41:50 +0100

    Initial CDK project setup

--- bin/cdk-github.ts
+++ bin/cdk-github.ts
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { CdkGithubStack } from '../lib/cdk-github-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Ec2Stack } from '../lib/ec2-stack';
 
 const app = new cdk.App();
-new CdkGithubStack(app, 'CdkGithubStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Environment (needed for default VPC lookup)
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+new Ec2Stack(app, 'Ec2Stack', { env });
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
---
+++ lib/ec2-stack.ts
@@ -0,0 +1,37 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export class Ec2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Use the default VPC
+    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', {
+      isDefault: true,
+    });
+
+    // Create a custom security group
+    const sg = new ec2.SecurityGroup(this, 'MySecurityGroup', {
+      vpc,
+      allowAllOutbound: true,
+      securityGroupName: 'ec2-sg',
+    });
+
+    // Open only SSH port for now
+    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH');
+
+    // Use existing AWS keypair
+    const keyPair = ec2.KeyPair.fromKeyPairName(this, 'KeyPair', 'ec2-demo-key');
+
+    // Create EC2 instance attached to the custom SG
+    new ec2.Instance(this, 'DemoInstance', {
+      vpc,
+      instanceType: new ec2.InstanceType('t3.micro'),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      keyPair: keyPair,
+      securityGroup: sg,         // 🔥 Attach custom SG
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+  }
+}
